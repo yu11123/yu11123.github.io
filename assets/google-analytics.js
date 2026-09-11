c9fdd6c320e09f31a415a79d580e610596116158
yu11123 <96585517+yu11123@users.noreply.github.com>
2026-09-11 21:07:27 +0800

    Add Google Analytics tracking

---
+++ assets/google-analytics.js
@@ -0,0 +1,7 @@
+window.dataLayer = window.dataLayer || [];
+window.gtag = function gtag() {
+  window.dataLayer.push(arguments);
+};
+
+window.gtag('js', new Date());
+window.gtag('config', 'G-XL81V7DV9M');
